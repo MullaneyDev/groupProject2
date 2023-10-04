b7e1108df123ce8f465e8230760ef7291e21ba20
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-04 15:30:16 +0100

    addition of css and move of cat array to app component

--- cats4lyf/src/App.js
+++ cats4lyf/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cats from "./pages/cats";
 import Home from "./pages/home";
 import Checkout from "./pages/checkout";
@@ -8,23 +8,44 @@ import "./App.css";
 
 function App() {
   const [checkout, setCheckout] = useState([]);
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    console.log(cats);
+    const fetchData = async () => {
+      const catRequest = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=10"
+      );
+      const catData = await catRequest.json();
+      setCats(catData);
+    };
+    fetchData();
+  }, []);
 
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/cats">Cats</Link>
-        <Link to="/checkout">Checkout</Link>
+        <Link to="/cats">Cats 🐱</Link>
+        <Link to="/checkout">🛒</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
           path="/cats"
-          element={<Cats checkout={checkout} setCheckout={setCheckout} />}
+          element={
+            <Cats checkout={checkout} setCheckout={setCheckout} cats={cats} />
+          }
         />
         <Route
           path="/checkout"
-          element={<Checkout checkout={checkout} setCheckout={setCheckout} />}
+          element={
+            <Checkout
+              checkout={checkout}
+              setCheckout={setCheckout}
+              cats={cats}
+            />
+          }
         />
       </Routes>
     </BrowserRouter>

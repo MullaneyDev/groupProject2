55c6945ad699c2713991e016bbdc01680d29d601
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-04 11:45:27 +0100

    addition of cats array

--- cats4lyf/src/App.js
+++ cats4lyf/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Cats from "./pages/cats";
 import Home from "./pages/home";
@@ -10,7 +10,29 @@ import "./App.css";
 function App() {
   const [checkout, setCheckout] = useState([]);
 
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cats">Cats</Link>
+        <Link to="/checkout">Checkout</Link>
+      </nav>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home checkout={checkout} setCheckout={setCheckout} />}
+        />
+        <Route
+          path="/cats"
+          element={<Cats checkout={checkout} setCheckout={setCheckout} />}
+        />
+        <Route
+          path="/checkout"
+          element={<Checkout checkout={checkout} setCheckout={setCheckout} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

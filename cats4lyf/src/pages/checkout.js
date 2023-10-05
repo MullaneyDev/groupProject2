a345baa1ee49e210de8e30cb54b28265b61c75ff
Nathan Stringer <nathstringer@gmail.com>
2023-10-05 08:04:26 +0100

    checkout with cat info and img, no modal

--- cats4lyf/src/pages/checkout.js
+++ cats4lyf/src/pages/checkout.js
@@ -23,22 +23,21 @@ const CheckoutPage = ({ checkout, setCheckout, cats }) => {
       {checkout.map((checkItem, index) => {
         return (
           <>
-            <div key={index}>
+            <div key={index} className="">
               <img
                 className="catImage"
                 onClick={openModal2}
                 src={checkItem.url}
                 alt="cats"
               ></img>
+              <h3>{checkItem.name}</h3>
+              <h1>{checkItem.price}</h1>
+              <h5>{checkItem.breed}</h5>
+              <h5>{checkItem.gender}</h5>
+              <button onClick={() => removeItem(index)}>
+                Remove from cart
+              </button>
             </div>
-            <Modal
-              className="ModalStyle"
-              isOpen={modal}
-              onRequestClose={closeModal}
-            >
-              <img className="catImage" src={checkout.url} alt="catimage"></img>
-              <button onClick={() => removeItem(index)}>Delete Item</button>
-            </Modal>
           </>
         );
       })}
